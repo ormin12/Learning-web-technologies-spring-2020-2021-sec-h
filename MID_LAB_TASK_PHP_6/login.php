f977f433aacb428354da26295030b59aa1d02179
ormin12 <77983143+ormin12@users.noreply.github.com>
2021-02-23 12:48:26 +0600

    Add files via upload

---
+++ MID_LAB_TASK_PHP_6/login.php
@@ -0,0 +1,35 @@
+<body>
+	
+	
+	
+	<form action="" method="">
+	
+	<fieldset >
+	<legend>LOGIN</legend>
+	<table >
+	<tr>
+	<td>User name</td>
+	<td>:<input type="text" name="name" value=""></td>
+	</tr>
+	<tr>
+	<td>Password</td>
+	<td>:<input type="Password"  name="pass"></td>
+	
+	</tr>
+	
+ <tr>
+ <td>Remember me</td>
+ <td><input type="checkbox" name="item[]"/></td>
+ 
+ </tr>
+ <tr>
+	<td><input type="submit" name="" value="Submit"/> </td>
+	<td><a href="">Forget Password?</a></td>
+
+ </tr> 
+	
+	</table>
+	</fieldset>
+	</form>
+	</body>
+	
---
+++ MID_LAB_TASK_PHP_6/reg.php
@@ -0,0 +1,71 @@
+<html>
+	<head>
+	<title></title>
+		</head>
+		<body>
+		
+		<form method=""  action="">
+		
+		<fieldset>
+		<legend>Registration</legend>
+		<table>
+		
+		<tr>
+			<td>Name</td>
+			<td><input type="text" name="name" value="" placeholder=""/> </td>
+		
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td><input type="email" name="email" value=""/></td>
+			<td>
+		
+		</tr>
+		
+		<tr>
+			<td>Username</td>
+			<td><input type="text" name="username" value="" placeholder=""/> </td>
+		
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="password" name="password" value="" placeholder=""/> </td>
+		
+		</tr>
+		<tr>
+			<td> Confirm Password</td>
+			<td><input type="password" name="password" value="" placeholder=""/> </td>
+		
+		</tr>
+		<tr>
+			<td> Date of Birth</td>
+			<td><input type="date" name="date" value="" /> </td>
+		
+		</tr>
+		<tr>
+		<td>Gender</td>
+		<td>
+			<input type="radio" name="gender"/>Male
+			<input type="radio" name="gender"/>Female
+			 <input type="radio" name="gender"/>Other
+		 </td>
+		</tr>
+		<tr>
+		<td><input type="submit" name="" value="Submit">
+	       <input type="reset" name="" value="Reset">
+			</td>
+			</tr>
+		
+		
+		
+		</table>
+			
+					
+					</fieldset>
+					
+		
+		</form>
+		
+		</body>
+		
+</html>	
